8fb2cd309865764929684e48695c45bb4054e355
Iamerroralpha <goubiah@gmail.com>
2024-11-11 17:48:14 -0600

    improved readability

--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,96 @@ use quick_xml::events::Event;
 use quick_xml::name::QName;
 use quick_xml::reader::Reader;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_name  = "test_data/T113_0.xml";
+#[derive(Default, Debug)]
+struct CfdiComprobante {
+    folio: String,
+    fecha: String,
+    moneda: String,
+    total: String,
+    sello: String,
+    no_certificado: String,
+    sub_total: String,
+    version: String,
+    metodo_pago: String,
+    forma_pago: String,
+    tipo_de_comprobante: String,
+    certificado: String,
+    lugar_expedicion: String,
+    exportacion: String,
+}
 
-    let mut file = File::open(file_name)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    println!("File: {}", file_name);
-    //println!("Contents: {}", contents);
+impl CfdiComprobante {
+    fn display(&self) {
+        println!("Folio: {}", self.folio);
+        println!("Fecha: {}", self.fecha);
+        println!("Moneda: {}", self.moneda);
+        println!("Total: {}", self.total);
+        println!("Sello: {}", self.sello);
+        println!("NoCertificado: {}", self.no_certificado);
+        println!("SubTotal: {}", self.sub_total);
+        println!("Version: {}", self.version);
+        println!("MetodoPago: {}", self.metodo_pago);
+        println!("FormaPago: {}", self.forma_pago);
+        println!("TipoDeComprobante: {}", self.tipo_de_comprobante);
+        println!("Certificado: {}", self.certificado);
+        println!("LugarExpedicion: {}", self.lugar_expedicion);
+        println!("Exportacion: {}", self.exportacion);
+    }
+}
 
-    let mut reader = Reader::from_str(&contents);
+struct CfdiParser {
+    file_name: String,
+}
 
-    let mut folio = String::new();
-    let mut fecha = String::new();
-    let mut moneda = String::new();
-    let mut total = String::new();
-    let mut sello = String::new();
-    let mut no_certificado = String::new();
+impl CfdiParser {
+    fn new(file_name: &str) -> Self {
+        CfdiParser { file_name: file_name.to_string() }
+    }
 
-    loop {
-        match reader.read_event() {
-            Ok(Event::Start(ref e)) => {
-                if e.name() == QName(b"cfdi:Comprobante") {
-                    for attr in e.attributes() {
-                        if let Ok(attr) = attr {
-                            match attr.key {
-                                QName(b"Folio") => folio = String::from_utf8_lossy(&attr.value).into_owned(),
-                                QName(b"Fecha") => fecha = String::from_utf8_lossy(&attr.value).into_owned(),
-                                QName(b"Moneda") => moneda = String::from_utf8_lossy(&attr.value).into_owned(),
-                                QName(b"Total") => total = String::from_utf8_lossy(&attr.value).into_owned(),
-                                QName(b"Sello") => sello = String::from_utf8_lossy(&attr.value).into_owned(),
-                                QName(b"NoCertificado") => no_certificado = String::from_utf8_lossy(&attr.value).into_owned(),
-                                _ => (),
-                            }
+    fn parse(&self) -> Result<CfdiComprobante, Box<dyn std::error::Error>> {
+        let mut contents = String::new();
+        File::open(&self.file_name)?.read_to_string(&mut contents)?;
+        
+        let mut reader = Reader::from_str(&contents);
+        let mut comprobante = CfdiComprobante::default();
+
+        loop {
+            match reader.read_event() {
+                Ok(Event::Start(ref e)) if e.name() == QName(b"cfdi:Comprobante") => {
+                    for attr in e.attributes().flatten() {
+                        let value = String::from_utf8_lossy(&attr.value).into_owned();
+                        match attr.key {
+                            QName(b"Folio") => comprobante.folio = value,
+                            QName(b"Fecha") => comprobante.fecha = value,
+                            QName(b"Moneda") => comprobante.moneda = value,
+                            QName(b"Total") => comprobante.total = value,
+                            QName(b"Sello") => comprobante.sello = value,
+                            QName(b"NoCertificado") => comprobante.no_certificado = value,
+                            QName(b"SubTotal") => comprobante.sub_total = value,
+                            QName(b"Version") => comprobante.version = value,
+                            QName(b"MetodoPago") => comprobante.metodo_pago = value,
+                            QName(b"FormaPago") => comprobante.forma_pago = value,
+                            QName(b"TipoDeComprobante") => comprobante.tipo_de_comprobante = value,
+                            QName(b"Certificado") => comprobante.certificado = value,
+                            QName(b"LugarExpedicion") => comprobante.lugar_expedicion = value,
+                            QName(b"Exportacion") => comprobante.exportacion = value,
+                            _ => (),
                         }
                     }
                 }
+                Ok(Event::Eof) => break,
+                Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                _ => (),
             }
-            Ok(Event::Eof) => break,
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-            _ => (),
         }
+
+        Ok(comprobante)
     }
+}
 
-    println!("NoCertificado: {}", no_certificado);
-    println!("Folio: {}", folio);
-    println!("Fecha: {}", fecha);
-    println!("Moneda: {}", moneda);
-    println!("Total: {}", total);
-    println!("Sello: {}", sello);
-
-
-
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let parser = CfdiParser::new("test_data/T113_0.xml");
+    let comprobante = parser.parse()?;
+    comprobante.display();
     Ok(())
 }
