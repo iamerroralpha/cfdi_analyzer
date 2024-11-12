4e9698b4b9d914f72b57e80394f1687033f8f416
Iamerroralpha <goubiah@gmail.com>
2024-11-12 15:22:14 -0600

    structs properly fillng

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+mod models;
+
 use minidom::Element;
 use std::fs::File;
 use std::io::Read;
 use std::error::Error;
+use models::comprobante::{Comprobante, Emisor, Receptor, Concepto, Impuestos, Traslado, TimbreFiscalDigital};
 
 fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
     // Step 1: Load the XML file content into a string
@@ -16,6 +19,7 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
         println!("Expected root element 'Comprobante', found '{}'", root.name());
         return Ok(());
     }
+    let mut comprobante = Comprobante::default();
     println!("------------------------------------------------------------------------");
     println!("Root Complemento: {}", root.name());
     // Step 4: Extract attributes from the "Comprobante" root element
@@ -25,6 +29,21 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
         "FormaPago", "Fecha", "TipoDeComprobante", "Certificado",
         "LugarExpedicion", "Moneda", "Sello", "Exportacion"
     ];
+
+    comprobante.version = root.attr("Version").unwrap().to_string();
+    comprobante.serie = root.attr("Serie").unwrap().to_string();
+    comprobante.folio = root.attr("Folio").unwrap().to_string();
+    comprobante.fecha = root.attr("Fecha").unwrap().to_string();
+    comprobante.sello = root.attr("Sello").unwrap().to_string();
+    comprobante.forma_pago = root.attr("FormaPago").unwrap().to_string();
+    comprobante.no_certificado = root.attr("NoCertificado").unwrap().to_string();
+    comprobante.certificado = root.attr("Certificado").unwrap().to_string();
+    comprobante.sub_total = root.attr("SubTotal").unwrap().to_string();
+    comprobante.total = root.attr("Total").unwrap().to_string();
+    comprobante.tipo_de_comprobante = root.attr("TipoDeComprobante").unwrap().to_string();
+    comprobante.metodo_pago = root.attr("MetodoPago").unwrap().to_string();
+    comprobante.lugar_expedicion = root.attr("LugarExpedicion").unwrap().to_string();
+
     for attr in &comprobante_attrs {
         if let Some(value) = root.attr(attr) {
             println!("{} = {}", attr, value);
@@ -36,6 +55,15 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
     // Step 5: Process the "Emisor" child
     println!("------------------------------------------------------------------------");
     println!("Processing Emisor");
+
+    let emisor = Emisor {
+        rfc: root.get_child("Emisor", root_namespace).unwrap().attr("Rfc").unwrap().to_string(),
+        nombre: root.get_child("Emisor", root_namespace).unwrap().attr("Nombre").unwrap().to_string(),
+        regimen_fiscal: root.get_child("Emisor", root_namespace).unwrap().attr("RegimenFiscal").unwrap().to_string(),
+    };
+
+    comprobante.emisor = emisor;
+
     if let Some(emisor) = root.get_child("Emisor", root_namespace) {
         let emisor_attrs = ["RegimenFiscal", "Rfc", "Nombre"];
         println!("Emisor attributes:");
@@ -48,6 +76,17 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
     // Step 6: Process the "Receptor" child
     println!("------------------------------------------------------------------------");
     println!("Processing Receptor");
+
+    let receptor = Receptor {
+        rfc: root.get_child("Receptor", root_namespace).unwrap().attr("Rfc").unwrap().to_string(),
+        nombre: root.get_child("Receptor", root_namespace).unwrap().attr("Nombre").unwrap().to_string(),
+        uso_cfdi: root.get_child("Receptor", root_namespace).unwrap().attr("UsoCFDI").unwrap().to_string(),
+        domicilio_fiscal: root.get_child("Receptor", root_namespace).unwrap().attr("DomicilioFiscalReceptor").unwrap().to_string(),
+        regimen_fiscal: root.get_child("Receptor", root_namespace).unwrap().attr("RegimenFiscalReceptor").unwrap().to_string(),
+    };
+
+    comprobante.receptor = receptor;
+
     if let Some(receptor) = root.get_child("Receptor", root_namespace) {
         let receptor_attrs = ["Nombre", "UsoCFDI", "DomicilioFiscalReceptor", "Rfc", "RegimenFiscalReceptor"];
         println!("Receptor attributes:");
@@ -63,6 +102,16 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
     println!("Processing Conceptos");
     if let Some(conceptos) = root.get_child("Conceptos", root_namespace) {
         for concepto in conceptos.children().filter(|c| c.name() == "Concepto") {
+
+            let mut concepto_obj = Concepto::default();
+            concepto_obj.objeto_imp = concepto.attr("ObjetoImp").unwrap().to_string();
+            concepto_obj.valor_unitario = concepto.attr("ValorUnitario").unwrap().to_string();
+            concepto_obj.importe = concepto.attr("Importe").unwrap().to_string();
+            concepto_obj.clave_prod_serv = concepto.attr("ClaveProdServ").unwrap().to_string();
+            concepto_obj.descripcion = concepto.attr("Descripcion").unwrap().to_string();
+            concepto_obj.cantidad = concepto.attr("Cantidad").unwrap().to_string();
+            concepto_obj.clave_unidad = concepto.attr("ClaveUnidad").unwrap().to_string();
+
             let concepto_attrs = ["ObjetoImp", "ValorUnitario", "Importe", "ClaveProdServ", "Descripcion", "Cantidad", "ClaveUnidad"];
             println!("\tConcepto attributes:");
             for attr in &concepto_attrs {
@@ -74,7 +123,19 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
             // Process "Traslado" under each "Concepto"
             if let Some(impuestos) = concepto.get_child("Impuestos", root_namespace) {
                 if let Some(traslados) = impuestos.get_child("Traslados", root_namespace) {
+                    
                     for traslado in traslados.children().filter(|t| t.name() == "Traslado") {
+
+                        let mut traslado_obj = Traslado::default();
+
+                        traslado_obj.tasa_o_cuota = traslado.attr("TasaOCuota").unwrap().to_string();
+                        traslado_obj.importe = traslado.attr("Importe").unwrap().to_string();
+                        traslado_obj.base = traslado.attr("Base").unwrap().to_string();
+                        traslado_obj.tipo_factor = traslado.attr("TipoFactor").unwrap().to_string();
+                        traslado_obj.impuesto = traslado.attr("Impuesto").unwrap().to_string();
+
+                        concepto_obj.impuestos.traslados.push(traslado_obj);
+
                         let traslado_attrs = ["TasaOCuota", "Importe", "Base", "TipoFactor", "Impuesto"];
                         println!("\t\tTraslado attributes:");
                         for attr in &traslado_attrs {
@@ -85,6 +146,8 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
                     }
                 }
             }
+            
+            comprobante.conceptos.push(concepto_obj);
         }
     }
 
@@ -92,6 +155,10 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
     println!("------------------------------------------------------------------------");
     println!("Processing Impuestos");
     if let Some(impuestos) = root.get_child("Impuestos", root_namespace) {
+        let mut impuestos_obj = Impuestos::default();
+
+        impuestos_obj.total_impuestos_trasladados = impuestos.attr("TotalImpuestosTrasladados").unwrap().to_string();
+
         if let Some(total_impuestos) = impuestos.attr("TotalImpuestosTrasladados") {
             println!("TotalImpuestosTrasladados = {}", total_impuestos);
         }
@@ -99,6 +166,17 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
         // Process "Traslado" under "Impuestos"
         if let Some(traslados) = impuestos.get_child("Traslados", root_namespace) {
             for traslado in traslados.children().filter(|t| t.name() == "Traslado") {
+
+                let mut traslado_obj = Traslado::default();
+
+                traslado_obj.tasa_o_cuota = traslado.attr("TasaOCuota").unwrap().to_string();
+                traslado_obj.importe = traslado.attr("Importe").unwrap().to_string();
+                traslado_obj.base = traslado.attr("Base").unwrap().to_string();
+                traslado_obj.tipo_factor = traslado.attr("TipoFactor").unwrap().to_string();
+                traslado_obj.impuesto = traslado.attr("Impuesto").unwrap().to_string();
+
+                impuestos_obj.traslados.push(traslado_obj);
+
                 let traslado_attrs = ["TasaOCuota", "Importe", "Base", "TipoFactor", "Impuesto"];
                 println!("Impuestos Traslado attributes:");
                 for attr in &traslado_attrs {
@@ -108,6 +186,8 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        comprobante.impuestos = impuestos_obj;
     }
 
     // Step 9: Process "Complemento" and "TimbreFiscalDigital"
@@ -115,6 +195,19 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
     println!("Processing Complemento and TimbreFiscalDigital");
     if let Some(complemento) = root.get_child("Complemento", root_namespace) {
         if let Some(timbre) = complemento.get_child("TimbreFiscalDigital", "http://www.sat.gob.mx/TimbreFiscalDigital") {
+
+            let mut timbre_obj = TimbreFiscalDigital::default();
+
+            timbre_obj.version = timbre.attr("Version").unwrap().to_string();
+            timbre_obj.no_certificado_sat = timbre.attr("NoCertificadoSAT").unwrap().to_string();
+            timbre_obj.fecha_timbrado = timbre.attr("FechaTimbrado").unwrap().to_string();
+            timbre_obj.rfc_prov_certif = timbre.attr("RfcProvCertif").unwrap().to_string();
+            timbre_obj.sello_cfd = timbre.attr("SelloCFD").unwrap().to_string();
+            timbre_obj.uuid = timbre.attr("UUID").unwrap().to_string();
+            timbre_obj.sello_sat = timbre.attr("SelloSAT").unwrap().to_string();
+
+            comprobante.timbre_fiscal_digital = timbre_obj;
+
             let timbre_attrs = ["Version", "NoCertificadoSAT", "FechaTimbrado", "RfcProvCertif", "SelloCFD", "UUID", "SelloSAT"];
             println!("TimbreFiscalDigital attributes:");
             for attr in &timbre_attrs {
@@ -129,6 +222,6 @@ fn explore_root(file_name: &str) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    explore_root("test_data/T113_0.xml")?;
+    explore_root("test_data/INV-INV2024080022-MX-Invoice-4.0.xml")?;
     Ok(())
 }
--- src/models/comprobante.rs
+++ src/models/comprobante.rs
@@ -1,3 +1,5 @@
+
+#[derive(Debug, Default)]
 pub struct Comprobante {
     pub version: String,
     pub serie: String,
@@ -16,18 +18,59 @@ pub struct Comprobante {
     pub receptor: Receptor,
     pub conceptos: Vec<Concepto>,
     pub impuestos: Impuestos,
+    pub timbre_fiscal_digital: TimbreFiscalDigital,
 }
 
+#[derive(Debug, Default)]
 pub struct Emisor {
     pub rfc: String,
     pub nombre: String,
     pub regimen_fiscal: String,
 }
 
+#[derive(Debug, Default)]
 pub struct Receptor {
     pub rfc: String,
     pub nombre: String,
     pub uso_cfdi: String,
     pub domicilio_fiscal: String,
     pub regimen_fiscal: String,
+}
+
+#[derive(Debug, Default)]
+pub struct Concepto {
+    pub objeto_imp: String,
+    pub valor_unitario: String,
+    pub importe: String,
+    pub clave_prod_serv: String,
+    pub descripcion: String,
+    pub cantidad: String,
+    pub clave_unidad: String,
+    pub impuestos: Impuestos,
+}
+
+#[derive(Debug, Default)]
+pub struct Impuestos {
+    pub total_impuestos_trasladados: String,
+    pub traslados: Vec<Traslado>,
+}
+
+#[derive(Debug, Default)]
+pub struct Traslado {
+    pub tasa_o_cuota: String,
+    pub importe: String,
+    pub base: String,
+    pub tipo_factor: String,
+    pub impuesto: String,
+}
+
+#[derive(Debug, Default)]
+pub struct TimbreFiscalDigital {
+    pub version: String,
+    pub uuid: String,
+    pub fecha_timbrado: String,
+    pub rfc_prov_certif: String,
+    pub sello_cfd: String,
+    pub no_certificado_sat: String,
+    pub sello_sat: String,
 }
